a44c79e9acd5a03791e93302f3ec7177573f06a7
KushalGoyal03 <kushalgoyal011@gmail.com>
2024-10-02 18:27:45 +0530

    Setup webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,15 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
 };
