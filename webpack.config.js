d28600068d34d03fdb9f31389a6feb3a3cdeb5de
KushalGoyal03 <kushalgoyal011@gmail.com>
2024-10-06 12:39:22 +0530

    Update webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
+// eslint-disable-next-line no-undef
 const path = require("path");
 
+// eslint-disable-next-line no-undef
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        // eslint-disable-next-line no-undef
+        path: path.resolve(__dirname, "dist"),
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
 };
